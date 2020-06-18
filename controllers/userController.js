7757a4ad0dc410a6593b9c3200c812c20402d661
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-06-18 07:07:12 +0530

    Modified Old Files

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,16 @@
 const User = require('../models/User');
+const User_dtl = require('../models/User_dtl');
+
+exports.mustBeLoggedIn = (req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    req.flash('errors', 'You must be logged in to perform this action.');
+    req.session.save(() => {
+      res.redirect('/');
+    });
+  }
+};
 
 exports.login = (req, res) => {
   let user = new User(req.body);
@@ -6,6 +18,7 @@ exports.login = (req, res) => {
     req.session.user =
     {
       username: user.data.username,
+      _id: user.data._id
     };
     req.session.save(() => {
       res.redirect('/');
@@ -24,6 +37,7 @@ exports.register = (req, res) => {
     req.session.user =
     {
       username: user.data.username,
+      _id: user.data._id
     };
     req.session.save(() => {
       res.redirect('/');
@@ -40,7 +54,7 @@ exports.register = (req, res) => {
 
 exports.home = (req, res) => {
   if (req.session.user) {
-    res.render('home-dashboard',
+    res.render('home-userDetails',
       {
         username: req.session.user.username
       });
@@ -56,5 +70,17 @@ exports.home = (req, res) => {
 exports.logout = (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
+  });
+};
+
+exports.userdtl = (req, res) => {
+  let user_dtl = new User_dtl(
+    req.body,
+    req.session.user._id
+  );
+  user_dtl.details().then(() => {
+    res.send('Details embedded');
+  }).catch((errors) => {
+    res.send(errors);
   });
 };
--- routes.js
+++ routes.js
@@ -6,5 +6,13 @@ routes.get('/', userController.home);
 routes.post('/register', userController.register);
 routes.post('/login', userController.login);
 routes.post('/logout', userController.logout);
+// routes.get('/user-details',
+//   userController.mustBeLoggedIn,
+//   userController.user_details
+// );
+routes.post('/user-details',
+  userController.mustBeLoggedIn,
+  userController.userdtl
+)
 
 module.exports = routes;
